276db4bc5eb96f1a77ac2a58b6f0ec3b1e80ceca
rmsivasankar <rmsivasankar@protonmail.com>
2025-03-19 02:59:06 +0530

    New Final ++

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ['static.wikia.nocookie.net'], // Add your external image domains here
   },
